14a4644327c530d639e395189fd9bf62fcc4d502
Falalatina <karolina40.gorska17@gmail.com>
2023-09-19 14:50:10 +0200

    edit

---
+++ React-app/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import PplList from "./modules/ppl-list/ppl-list";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <PplList />
+    </div>
+  );
+}
+
+export default App;
