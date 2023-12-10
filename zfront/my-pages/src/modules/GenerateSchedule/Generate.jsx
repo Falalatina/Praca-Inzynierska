e492b63c6f1aab1e39e9ea8d3d543cc663b39106
Falalatina <karolina40.gorska17@gmail.com>
2023-12-11 00:12:48 +0100

    działa

--- zfront/my-pages/src/modules/GenerateSchedule/Generate.jsx
+++ zfront/my-pages/src/modules/GenerateSchedule/Generate.jsx
@@ -5,7 +5,75 @@ import { autoSchedule } from "../../features/team/generateSlice";
 
 const Generate = () => {
   const dispatch = useDispatch();
-  return <Button onClick={() => dispatch(autoSchedule())}>Generate</Button>;
+
+  const numberOfIterations = 100;
+  const numberOfParents = 10;
+  const mutate = 50;
+  const workers = [
+    { name: "a", preferences: { yes: ["pn1"], no: ["wt2"] } },
+    { name: "b", preferences: { yes: ["pn2"], no: ["wt3"] } },
+  ];
+  const population = [];
+  const workShifts = [
+    "pn1",
+    "pn2",
+    "pn3",
+    "wt1",
+    "wt2",
+    "wt3",
+    "sr1",
+    "sr2",
+    "sr3",
+    "czw1",
+    "czw2",
+    "czw3",
+    "pt1",
+    "pt2",
+    "pt3",
+  ];
+  const numberOfPersonOnShift = 3;
+
+  const randomElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+  const createStartingPopulation = (
+    numberOfParents,
+    workShifts,
+    numberOfPersonOnShift
+  ) => {
+    for (let index = 0; index < numberOfParents; index++) {
+      const chromosome = [];
+      for (let index = 0; index < workShifts.length; index++) {
+        const day = [];
+        const re1 = /.+[1-2]/;
+        const re3 = /.+[3]/;
+        if (workShifts[index].match(re1)) {
+          for (let index = 0; index < numberOfPersonOnShift; index++) {
+            day.push(randomElement(workers).name);
+          }
+        }
+        if (workShifts[index].match(re3)) {
+          for (let index = 0; index < numberOfPersonOnShift - 2; index++) {
+            day.push(randomElement(workers).name);
+          }
+        }
+        chromosome.push(day);
+      }
+      population.push(chromosome);
+    }
+  };
+
+  const startGenerate = () => {
+    console.log(population);
+    createStartingPopulation(
+      numberOfParents,
+      workShifts,
+      numberOfPersonOnShift
+    );
+    console.log(population);
+  };
+
+  return <Button onClick={() => startGenerate()}>Generate</Button>;
 };
 
 export default Generate;
