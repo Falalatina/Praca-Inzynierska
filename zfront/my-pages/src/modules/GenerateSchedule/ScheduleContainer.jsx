9f3568c81e7d52c8fa73dda4de8b809beb8343fd
Falalatina <karolina40.gorska17@gmail.com>
2023-12-20 23:06:00 +0100

    workin

--- zfront/my-pages/src/modules/GenerateSchedule/ScheduleContainer.jsx
+++ zfront/my-pages/src/modules/GenerateSchedule/ScheduleContainer.jsx
@@ -5,12 +5,8 @@ import ScheduleItem from "./ScheduleItem";
 
 const ScheduleContainer = ({ workShifts, name, id, graphic }) => {
   //console.log(name, id, graphic);
-  // console.log(graphic);
-  //console.log(graphic);
 
-  const firstShifts = [0, 3, 6, 9, 12];
-  const secondShifts = [1, 4, 7, 10, 13];
-  const thirdShifts = [2, 5, 8, 11, 14];
+  console.log(graphic);
 
   return (
     <>
@@ -21,38 +17,7 @@ const ScheduleContainer = ({ workShifts, name, id, graphic }) => {
       >
         <div className="shift-container">{name}</div>
 
-        {graphic.map((shift) => {
-          if (firstShifts.includes(shift)) {
-            return (
-              <div key={shift} className="shift-container">
-                <div className="first-shift">
-                  6&#58;00 &ndash; 14&#58;00
-                  <div className="tooltip">FIRST SHIFT</div>
-                </div>
-              </div>
-            );
-          }
-          if (secondShifts.includes(shift)) {
-            return (
-              <div key={shift} className="shift-container">
-                <div className="second-shift">
-                  14&#58;00 &ndash; 22&#58;00
-                  <div className="tooltip">SECOND SHIFT</div>
-                </div>
-              </div>
-            );
-          }
-          if (thirdShifts.includes(shift)) {
-            return (
-              <div key={shift} className="shift-container">
-                <div className="third-shift">
-                  22&#58;00 &ndash; 6&#58;00
-                  <div className="tooltip">THIRD SHIFT</div>
-                </div>
-              </div>
-            );
-          }
-        })}
+        <ScheduleItem graphic={graphic} />
       </Grid>
     </>
   );
--- zfront/my-pages/src/modules/GenerateSchedule/ScheduleItem.jsx
+++ zfront/my-pages/src/modules/GenerateSchedule/ScheduleItem.jsx
@@ -1,30 +1,44 @@
 import React from "react";
 
-const ScheduleItem = ({ bestSolution }) => {
-  // console.log(bestSolution);
-  const workers = [
-    { id: 1, name: "asia", preferences: { yes: ["pn1"], no: ["pn3", "wt3"] } },
-    { id: 2, name: "basia", preferences: { yes: ["wt1"], no: ["wt2"] } },
-    { id: 3, name: "kasia", preferences: { yes: ["sr1"], no: ["wt1"] } },
-    { id: 4, name: "kacper", preferences: { yes: ["czw1"], no: ["czw1"] } },
-  ];
+const ScheduleItem = ({ graphic }) => {
+  const firstShifts = [0, 3, 6, 9, 12];
+  const secondShifts = [1, 4, 7, 10, 13];
+  const thirdShifts = [2, 5, 8, 11, 14];
+
   return (
     <section>
-      {bestSolution[0] ? (
-        <div>
-          {bestSolution.map((item) => {
-            return (
-              <div>
-                {item[0].map((inner) => {
-                  return <div>{inner}</div>;
-                })}
+      {graphic.map((shift) => {
+        if (firstShifts.includes(shift)) {
+          return (
+            <div key={shift} className="shift-container">
+              <div className="first-shift">
+                6&#58;00 &ndash; 14&#58;00
+                <div className="tooltip">FIRST SHIFT</div>
               </div>
-            );
-          })}
-        </div>
-      ) : (
-        <></>
-      )}
+            </div>
+          );
+        }
+        if (secondShifts.includes(shift)) {
+          return (
+            <div key={shift} className="shift-container">
+              <div className="second-shift">
+                14&#58;00 &ndash; 22&#58;00
+                <div className="tooltip">SECOND SHIFT</div>
+              </div>
+            </div>
+          );
+        }
+        if (thirdShifts.includes(shift)) {
+          return (
+            <div key={shift} className="shift-container">
+              <div className="third-shift">
+                22&#58;00 &ndash; 6&#58;00
+                <div className="tooltip">THIRD SHIFT</div>
+              </div>
+            </div>
+          );
+        }
+      })}
     </section>
   );
 };
