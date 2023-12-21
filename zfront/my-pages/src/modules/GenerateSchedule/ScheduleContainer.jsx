94baee3995eca44a43ecbf83a78428f916240211
Falalatina <karolina40.gorska17@gmail.com>
2023-12-21 12:22:02 +0100

    wyswietlanie planu updated

--- zfront/my-pages/src/modules/GenerateSchedule/ScheduleContainer.jsx
+++ zfront/my-pages/src/modules/GenerateSchedule/ScheduleContainer.jsx
@@ -5,6 +5,36 @@ import ScheduleItem from "./ScheduleItem";
 
 const ScheduleContainer = ({ workShifts, name, id, graphic }) => {
   //console.log(name, id, graphic);
+  const valuesToCheck = ["pn", "wt", "sr", "czw", "pt"];
+
+  const findDay = (array, values) => {
+    return array.reduce((acc, shift, index) => {
+      values.forEach((value) => {
+        if (shift.includes(value)) {
+          acc[value] = [...(acc[value] || []), index];
+        }
+      });
+      return acc;
+    }, {});
+  };
+
+  const indexesOfDays = findDay(workShifts, valuesToCheck);
+
+  const isThatMonday = graphic.filter((element) =>
+    indexesOfDays["pn"].includes(element)
+  );
+  const isThatTuesday = graphic.filter((element) =>
+    indexesOfDays["wt"].includes(element)
+  );
+  const isThatWednesday = graphic.filter((element) =>
+    indexesOfDays["sr"].includes(element)
+  );
+  const isThatThursday = graphic.filter((element) =>
+    indexesOfDays["czw"].includes(element)
+  );
+  const isThatFriday = graphic.filter((element) =>
+    indexesOfDays["pt"].includes(element)
+  );
 
   console.log(graphic);
 
@@ -15,9 +45,15 @@ const ScheduleContainer = ({ workShifts, name, id, graphic }) => {
         templateColumns="repeat(6, 1fr)"
         gap={1}
       >
-        <div className="shift-container">{name}</div>
+        <div className="workers-container">
+          <div className="worker">{name}</div>
+        </div>
 
-        <ScheduleItem graphic={graphic} />
+        <ScheduleItem graphic={isThatMonday} />
+        <ScheduleItem graphic={isThatTuesday} />
+        <ScheduleItem graphic={isThatWednesday} />
+        <ScheduleItem graphic={isThatThursday} />
+        <ScheduleItem graphic={isThatFriday} />
       </Grid>
     </>
   );
