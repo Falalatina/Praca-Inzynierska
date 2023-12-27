287358e0649211be2f3982bad08bb6f84e57f08d
Falalatina <karolina40.gorska17@gmail.com>
2023-12-28 00:54:52 +0100

    Update modalSlice.jsx

--- zfront/my-pages/src/features/team/modalSlice.jsx
+++ zfront/my-pages/src/features/team/modalSlice.jsx
@@ -52,10 +52,10 @@ const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal: (state, action) => {
+    openModal: (state) => {
       state.isOpen = true;
     },
-    closeModal: (state, action) => {
+    closeModal: (state) => {
       state.isOpen = false;
     },
     updateHourOfStart: (state, action) => {
