12358fa17e1252cb0d8004ef8b8238a0f19cf2f3
Falalatina <karolina40.gorska17@gmail.com>
2024-01-04 18:48:04 +0100

    get selected user id

--- zfront/my-pages/src/modules/UserList/AddUser.jsx
+++ zfront/my-pages/src/modules/UserList/AddUser.jsx
@@ -1,63 +1,40 @@
 import React from "react";
 
-import { ScaleFade } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+
+import {
+  ScaleFade,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Select,
+  Input,
+} from "@chakra-ui/react";
 
 const AddUser = ({ toggle }) => {
+  const { workers } = useSelector((store) => store.generate);
+
   const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
     e.preventDefault();
-    if (!user.name) return;
-    const fakeId = Date.now();
-    const name = user.name;
-    const stage = user.stage;
-    const shifts = user.shifts;
-    const newPerson = { id: fakeId, name, stage, shifts };
-    const updatePeople = [...people, newPerson];
-    setPeople(updatePeople);
-
-    // console.log(name);
-    setUser({ name: "", stage: "", shifts: "" });
+    console.log(e.target.value);
   };
 
   return (
     <ScaleFade initialScale={0.9} in={toggle}>
-      <form className="form" onSubmit={handleSubmit}>
-        <h3>Add User</h3>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            className="form-input"
-            id="name"
-            value={user.name}
-            onChange={handleChange}
-            name="name"
-          />
-        </div>
-
-        <div className="form-row">
-          <label htmlFor="stage" className="form-label">
-            stage
-          </label>
-          <input
-            type="text"
-            className="form-input"
-            id="stage"
-            value={user.stage}
-            onChange={handleChange}
-            name="stage"
-          />
-        </div>
-
-        <button type="submit" className="btn ">
-          submit
-        </button>
-      </form>
+      <FormControl>
+        <FormLabel>Select User</FormLabel>
+        <Select onChange={handleChange} placeholder="Select user">
+          {workers.map((user) => {
+            const { name, id } = user;
+            return (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            );
+          })}
+        </Select>
+      </FormControl>
     </ScaleFade>
   );
 };
---
+++ zfront/my-pages/src/modules/UserList/CreateUser.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { ScaleFade } from "@chakra-ui/react";
+
+const CreateUser = ({ toggle }) => {
+  // const handleChange = (e) => {
+  //   setUser({ ...user, [e.target.name]: e.target.value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (!user.name) return;
+  //   const fakeId = Date.now();
+  //   const name = user.name;
+  //   const stage = user.stage;
+  //   const shifts = user.shifts;
+  //   const newPerson = { id: fakeId, name, stage, shifts };
+  //   const updatePeople = [...people, newPerson];
+  //   setPeople(updatePeople);
+
+  //   // console.log(name);
+  //   setUser({ name: "", stage: "", shifts: "" });
+  // };
+
+  return (
+    <ScaleFade initialScale={0.9} in={toggle}>
+      <form className="form">
+        <h3>Add User</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input type="text" className="form-input" id="name" name="name" />
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="stage" className="form-label">
+            stage
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={user.name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+
+        <button type="submit" className="btn ">
+          submit
+        </button>
+      </form>
+    </ScaleFade>
+  );
+};
+
+export default CreateUser;
