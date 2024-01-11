77b773c52b4008e70dc09918f9ee36acadabb944
Falalatina <karolina40.gorska17@gmail.com>
2024-01-11 12:02:34 +0100

    add some stylin

--- zfront/my-pages/src/modules/User/UserChoose.jsx
+++ zfront/my-pages/src/modules/User/UserChoose.jsx
@@ -1,6 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchWorkers } from "../../features/team/userSlice";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Grid,
+  GridItem,
+  Text,
+} from "@chakra-ui/react";
+
+import "./userStyle.css";
 
 const UserChoose = () => {
   const { workers } = useSelector((store) => store.user);
@@ -15,12 +25,24 @@ const UserChoose = () => {
   }, [workers]);
 
   return (
-    <div>
-      <h1>Hi</h1>
-      {workers.map((person) => {
-        return <div>{person.name}</div>;
-      })}
-    </div>
+    <section>
+      <h1>Check the user</h1>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        {workers.map((person) => {
+          const { id, name, hoursSum } = person;
+          return (
+            <GridItem className="user-card" key={id}>
+              <Card>
+                <CardHeader>
+                  <Text>{name}</Text>
+                </CardHeader>
+                <CardBody>{hoursSum}</CardBody>
+              </Card>
+            </GridItem>
+          );
+        })}
+      </Grid>
+    </section>
   );
 };
 
